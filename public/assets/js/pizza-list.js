7d965ace1228a2a0da168a9250a96e5b929bfd86
tonito98 <86890715+tonito98@users.noreply.github.com>
2022-08-29 18:07:57 +0200

    integrated the front-end and refactored code

--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -1,4 +1,16 @@
 const $pizzaList = document.querySelector('#pizza-list');
+const getPizzaList = () => {
+  fetch('/api/pizzas')
+  .then(response => response.json())
+  .then(pizzaListArr => {
+    pizzaListArr.forEach(printPizza);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+};
+
+
 
 const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, createdAt }) => {
   const pizzaCard = `
@@ -26,3 +38,6 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
 
   $pizzaList.innerHTML += pizzaCard;
 };
+
+// Call the function getPizzaList() on load
+getPizzaList();
